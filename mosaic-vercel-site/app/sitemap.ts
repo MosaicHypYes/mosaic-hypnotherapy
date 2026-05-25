3d9aa29398daa6fc633e90d4e704da0eef128b66
Mosaic Hypnotherapy <helen.ogorman@gmail.com>
2026-05-25 15:29:06 +0530

    Update sitemap.ts

--- mosaic-vercel-site/app/sitemap.ts
+++ mosaic-vercel-site/app/sitemap.ts
@@ -1,1 +1,16 @@
-
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.mosaichypnotherapy.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.mosaichypnotherapy.com/privacy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.mosaichypnotherapy.com/credentials",
+      lastModified: new Date(),
+    },
+  ];
+}
