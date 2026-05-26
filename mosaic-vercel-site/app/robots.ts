f601ab1c57b35b7155be49b384f568b04bdbaab4
Mosaic Hypnotherapy <helen.ogorman@gmail.com>
2026-05-26 13:14:40 +0530

    Update robots.ts

--- mosaic-vercel-site/app/robots.ts
+++ mosaic-vercel-site/app/robots.ts
@@ -1,1 +1,9 @@
-
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://www.mosaichypnotherapy.com/sitemap.xml",
+  };
+}
