3529fb7bdc76b8d817f4a87e5020a9713934731e
Mosaic Hypnotherapy <helen.ogorman@gmail.com>
2026-05-26 11:48:22 +0530

    Update Header.tsx

--- mosaic-vercel-site/components/Header.tsx
+++ mosaic-vercel-site/components/Header.tsx
@@ -7,17 +7,17 @@ export default function Header() {
       <div className="container-custom flex items-center justify-between py-5">
         <div className="flex items-center gap-4">
           <Image src="/logo.jpg" alt="Mosaic Logo" width={70} height={70} className="rounded-full" />
-          <div>
-            <h1 className="text-3xl tracking-widest">MOSAIC</h1>
-            <p className="tracking-[0.4em] text-sm">HYPNOTHERAPY</p>
-          </div>
+         <div className="hidden sm:block"> 
+         <h1 className="text-3xl tracking-widest">MOSAIC</h1>
+         <p className="tracking-[0.4em] text-sm">HYPNOTHERAPY</p>
+        </div>
         </div>
 
         <a
   href="https://calendly.com/helen-ogorman/new-meeting?hide_event_type_details=1&hide_gdpr_banner=1"
   target="_blank"
   rel="noopener noreferrer"
-  className="bg-deep text-white px-6 py-3 rounded-xl"
+  className="bg-deep text-white px-4 py-3 sm:px-6 sm:py-3 rounded-xl text-sm sm:text-base text-center"
 >
   Book a Free Consultation
 </a>
